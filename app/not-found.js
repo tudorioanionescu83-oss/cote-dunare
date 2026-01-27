eecbb12b321dafd59a92c63bdb6bd93923a8fec7
tudorioanionescu83-oss <tudorioanionescu83@gmail.com>
2026-01-27 12:14:06 +0200

    Create not-found.js

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>404 - Pagina nu există</h1>
+      <p>Custom 404 page</p>
+    </div>
+  );
+}
